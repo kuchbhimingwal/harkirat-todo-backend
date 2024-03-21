43c52ae98423efe976b42745bf4e5c2f1fb642fe
kuchbhimingwal <shubhammingi@gmail.com>
2024-03-21 00:49:16 -0400

    created a boilerplate code for express file

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+/// write basic express boilerplate code
+// with express.json() middleware
+
+const express = require("express");
+const app = express();
+
+app.use(express.json())
+
+app.post("/todo", (req, res)=>{
+  
+})
+
+app.get("/todos", (req, res)=>{
+
+})
+
+app.put("/completes", (req, res)=>{
+
+})
