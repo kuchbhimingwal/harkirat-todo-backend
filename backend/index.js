7080145883d894cfa8fc2ff7cccd7b821199f9a6
kuchbhimingwal <shubhammingi@gmail.com>
2024-03-21 22:53:33 -0400

    did some validation for input in body for todo

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,36 @@
 // with express.json() middleware
 
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 app.use(express.json())
 
+
 app.post("/todo", (req, res)=>{
-  
+  const createPayload = req.body;
+  const parsePayload = createTodo.safeParse(createPayload);
+  if(!parsePayload.success){
+    res.status(411).json({
+      msg: "you sent the wrong inputs"
+    })
+    return;
+  }
+  // put in mongodb
 })
 
 app.get("/todos", (req, res)=>{
 
 })
 
-app.put("/completes", (req, res)=>{
-
+app.put("/completed", (req, res)=>{
+  const createPayload = req.body;
+  const parsePayload = updateTodo.safeParse(createPayload);
+  if(!parsePayload.success){
+    res.status(411).json({
+      msg: "wrong in put"
+    })
+    return;
+  }
+  // mark as complete in mongon db
 })
