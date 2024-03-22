1dfc02a73e565d1d11350577b98f72fd361d52ea
kuchbhimingwal <shubhammingi@gmail.com>
2024-03-22 01:07:49 -0400

    added a put request to update if the todo is completed by clicking the todo button

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -9,7 +9,19 @@ function Todos({todos}) {
           <h2>{todo.description}</h2>
 
           <button key={todo._id} onClick={()=>{
-            fetch('http://localhost:3000/todo')
+            fetch('http://localhost:3000/completed',{
+              method: "PUT",
+              body: JSON.stringify({
+                id: todo._id,
+              }),
+              headers: {
+                "content-type": "application/json"
+              }
+            })
+            .then(async(res)=>{
+              const json = await res.json();
+              console.log(json);
+            })
           }}>{todo.completed == true ? "completed" : "mark as completed"}</button>
         </div>
       ))}
